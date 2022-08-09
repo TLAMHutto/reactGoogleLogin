ce3afffb175df4f049435790cdcb8bf1d8cdc770
TLAMHutto <keatonhutto8@gmail.com>
2022-08-09 02:32:02 -0500

    login alert

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { render } from 'react-dom'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId='1019671938210-kj3dodqde9u2str67ajqomlk3b8t6suj.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <AlertProvider template={AlertTemplate} {...options}>
     <Login />
+    </AlertProvider>
     </GoogleOAuthProvider>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,32 @@
 import { GoogleLogin } from '@react-oauth/google';
-
-
-
+import { useGoogleLogin } from '@react-oauth/google';
+import React, { useState } from "react";
+import { Button} from 'react-bootstrap';
+import { useAlert } from 'react-alert'
 
 function Login (){
+    const alert = useAlert()
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => (tokenResponse.accessToken !== '') ? alert.success('Login Successful') : alert.error('Login Failed'),
 
+      });
+      
 return(
+    <>
     <div>
-        <GoogleLogin
+        <Button onClick={login}>
+        
+            Login with Google
+        </Button>
+           <GoogleLogin
             onSuccess={credentialResponse => {
-            console.log('Loged in!');
+            console.log(credentialResponse);
                 }}
-        onError={() => {
+            onError={() => {
             console.log('Login Failed');
-                 }}/>;
-                    
+                 }}/>;         
     </div>
+    </>
 )
 }
 export default Login;

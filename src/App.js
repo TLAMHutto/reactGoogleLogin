1e7c6a1c525253157bfdb8e63f59aeb62712b10c
TLAMHutto <keatonhutto8@gmail.com>
2022-08-09 01:48:07 -0500

    working login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
+    <GoogleOAuthProvider clientId='1019671938210-kj3dodqde9u2str67ajqomlk3b8t6suj.apps.googleusercontent.com'>
     <Login />
+    </GoogleOAuthProvider>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,20 @@
-import {GoogleLogin} from 'react-google-login';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 
 
-const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-console.log(clientId)
+
+
 function Login (){
-    const onSuccess = (response) => {console.log('[Login Success] current user:', response.profileObj);}
-    const onFailure = (response) => {console.log('[Login Success] current user:', response);}
-
 
 return(
     <div>
         <GoogleLogin
-            clientId={clientId}
-            buttonText="Login"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy={'single_host_origin'}
-        />
+            onSuccess={credentialResponse => {
+            console.log('Loged in!');
+                }}
+        onError={() => {
+            console.log('Login Failed');
+                 }}/>;
+                    
     </div>
 )
 }
